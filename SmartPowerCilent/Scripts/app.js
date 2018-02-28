75ab6b5ca6a4096a4b68df50e799b3a7b70d377a
1601595j <34340727+1601595j@users.noreply.github.com>
2018-02-28 11:17:41 +0800

    1
    1

--- SmartPowerCilent/Scripts/app.js
+++ SmartPowerCilent/Scripts/app.js
@@ -1,12 +1,13 @@
 ﻿var ViewModel = function () {
     var self = this;
-    self.products = ko.observableArray();
+    self.activities = ko.observableArray();
+    self.user = ko.observableArray();
+    self.CurrentActivities = ko.observableArray();
+    self.presences = ko.observableArray();
     self.error = ko.observable();
+    self.details = ko.observable();
 
-
-    
-
-    var ActivitiesURI = 'http://localhost:60854/api/Activity';
+    var ActivitiesURI = 'http://localhost:60854/api/Activity/';
 
     function getAllActivities() {
         $.ajax({
@@ -15,7 +16,7 @@
             dataType: 'json',
             contentType: 'application/json',
             success: function (data) {
-                self.Activities(data);
+                self.activities(data);
             },
             error: function (err) {
                 alert("Error: " + err.status + " " + err.statusText);
@@ -24,7 +25,7 @@
     }
     getAllActivities();
 
-    var UsersURI = 'http://localhost:60854/api/Users';
+    var UsersURI = 'http://localhost:60854/api/Users/';
 
     function getAllUsers() {
         $.ajax({
@@ -33,7 +34,7 @@
             dataType: 'json',
             contentType: 'application/json',
             success: function (data) {
-                self.Users(data);
+                self.user(data);
             },
             error: function (err) {
                 alert("Error: " + err.status + " " + err.statusText);
@@ -41,15 +42,16 @@
         });
     }
 
+
     getAllUsers();
 
 
-    var CurrentActivitiesURI = 'http://localhost:60854/api/currentactivities';
+    var CurrentActivitiesURL = 'http://localhost:60854/api/currentactivities/';
 
     function getAllCurrentActivities() {
         $.ajax({
             type: 'GET',
-            url: CurrentActivitiesURI,
+            url: CurrentActivitiesURL,
             dataType: 'json',
             contentType: 'application/json',
             success: function (data) {
@@ -63,7 +65,7 @@
     getAllCurrentActivities();
 
 
-    var presencesURI = 'http://localhost:60854/api/Presence';
+    var presencesURI = 'http://localhost:60854/api/Presence/';
 
     function getAllpresences() {
         $.ajax({
@@ -82,7 +84,116 @@
 
     getAllpresences();
 
-    
+    self.getActivityDetails = function (item) {
+        $.ajax({
+            type: 'GET',
+            url: CurrentActivitiesURL +"/"+ item.CurrentActivityId,
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.details(data);
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+    };
+
+
+    self.removeCurrentActivites = function () {
+        var theActivity = {
+            CurrentActivityId: self.details.CurrentActivityId,
+            Lighting: self.details.Lighting,
+            Machine: self.details.Machine,
+            Alarm: self.details.Alarm,
+            Door: self.details.Door,
+            Timedate: self.details.Timedate
+        };
+        if (confirm('Are you sure to delete "' + theActivity.Timedate + '"?')) {
+            $.ajax({
+                type: 'DELETE',
+                url: CurrentActivitiesURL + theActivity.CurrentActivityId,
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (data) {
+                    self.activities.remove(theActivity);
+                    alert("Record has been deleted!");
+                },
+                error: function (err) {
+                    alert("Error: " + err.status + " " + err.statusText);
+                }
+            });
+        }
+    };
+
+    self.newActivity = {
+        CurrentActivityId: ko.observable(),
+        Lighting: ko.observable(),
+        Machine: ko.observable(),
+        Alarm: ko.observable(),
+        Door: ko.observable(),
+        Timedate: ko.observable()
+    };
+
+    self.addActivites = function () {
+        var anActivity = {
+            CurrentActivityId: self.newActivity.CurrentActivityId(),
+            Lighting: self.newActivity.Lighting(),
+            Machine: self.newActivity.Machine(),
+            Alarm: self.newActivity.Alarm(),
+            Door: self.newActivity.Door(),
+            Timedate: self.newActivity.Timedate()
+        };
+        $.ajax({
+            type: 'POST',
+            url: CurrentActivitiesURL,
+            data: JSON.stringify(anAntivity),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.CurrentActivities.push(data);
+                alert("Record created successfully!");
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+
+    };
+
+    self.saveActivity = function () {
+
+        var updateActivity = {
+            CurrentActivityId: self.details().CurrentActivityId,
+            Lighting: self.details().Lighting,
+            Machine: self.details().Machine,
+            Alarm: self.details().Alarm,
+            Door: self.details().Door,
+            Timedate: self.details().Timedate
+        };
+        $.ajax({
+            type: 'POST',
+            url: CurrentActivitiesURL + updateActivity.CurrentActivityId,
+            data: JSON.stringify(updateActivity),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.CurrentActivities.replace(data);
+                alert("Record updated successfully!");
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+
+    };
+
+
 };
 
-    ko.applyBindings(new ViewModel());
+
+
+
+
+
+ko.applyBindings(new ViewModel());
