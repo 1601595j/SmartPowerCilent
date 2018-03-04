c1a8cc3a4e1726da38bae99fd165363c9dcb63c3
changkw <kaiwenc716@gmail.com>
2018-03-04 22:31:43 +0800

    Client Final

--- SmartPowerCilent/Scripts/app.js
+++ SmartPowerCilent/Scripts/app.js
@@ -7,87 +7,6 @@
     self.error = ko.observable();
     self.details = ko.observable();
 
-
-    var UsersURI = 'http://localhost:60854/api/Users/';
-
-    function getAllUsers() {
-        $.ajax({
-            type: 'GET',
-            url: UsersURI,
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (data) {
-                self.user(data);
-            },
-            error: function (err) {
-                alert("Error: " + err.status + " " + err.statusText);
-            }
-        });
-    }
-
-
-    getAllUsers();
-
-    self.newUser = {
-        userId: ko.observable(),
-        email: ko.observable(),
-        user: ko.observable(),
-        password: ko.observable(),
-        number: ko.observable()
-    };
-
-    self.addUser = function () {
-        var addUser = {
-            userId: self.newUser.userId(),
-            email: self.newUser.email(),
-            user: self.newUser.user(),
-            password: self.newUser.password(),
-            number: self.newUser.number()
-        };
-        $.ajax({
-            type: 'POST',
-            url: UsersURL,
-            data: JSON.stringify(anAntivity),
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (data) {
-                self.CurrentActivities.push(data);
-                alert("User added successfully!");
-            },
-            error: function (err) {
-                alert("Error: " + err.status + " " + err.statusText);
-            }
-        });
-
-    };
-
-    self.removeUser = function () {
-        var theUser = {
-            userId: self.details.userId,
-            email: self.details.email,
-            user: self.details.user,
-            password: self.details.password,
-            number: self.details.number
-        };
-        if (confirm('Are you sure to delete "' + theUser.userid + '"?')) {
-            $.ajax({
-                type: 'DELETE',
-                url: UsersURL + theUser.userId,
-                dataType: 'json',
-                contentType: 'application/json',
-                success: function (data) {
-                    self.activities.remove(theUser);
-                    alert("Record has been deleted!");
-                },
-                error: function (err) {
-                    alert("Error: " + err.status + " " + err.statusText);
-                }
-            });
-        }
-    };
-
-
-
     var CurrentActivitiesURL = 'http://localhost:60854/api/currentactivities/';
 
     function getAllCurrentActivities() {
@@ -215,7 +134,7 @@
 
     // consume CardAccess tap in API
     self.tapIn = function (userid) {
-         $.ajax({
+        $.ajax({
             type: 'POST',
             url: worksURI + userid,
             success: function (data) {
@@ -242,8 +161,181 @@
             }
         });
     };
-    //
-};
 
 
+    //************  Remember to change the URI accordingly  **********************//
+    var NotificationsURI = 'http://localhost:60854/api/Notifications/';
+
+    //** this section contains all the AJAX call to the Web APIs **//
+    // function to retrieve all products using AJAX call to web API
+    function getAllNotifications() {
+        $.ajax({
+            type: 'GET',
+            url: NotificationsURI,
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.Notifications(data);
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+    };
+
+    // end of function to retrieve all products
+    //** this is the end of the section contains all the AJAX call to the Web APIs **//
+    self.getNotificationDetails = function (item) {
+        $.ajax({
+            type: 'GET',
+            url: NotificationsURI + item.NotificationID,
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.notificationdetails(data);
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+    };
+    // Fetch the initial data
+    getAllNotifications();
+
+
+    self.newNotifications = {
+        Id: ko.observable(),
+        timeDate: ko.observable()
+    };
+
+    self.addNotifications = function () {
+        var aNotifications = {
+            Id: self.newNotifications.Id(),
+            timeDate: self.newNotifications.timeDate(),
+        }
+
+
+        $.ajax({
+            type: 'POST',
+            url: NotificationsURI,
+            data: JSON.stringify(aNotifications),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.products.push(data);
+                alert("Record created successfully!");
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+    }
+
+
+    self.removeNotifications = function () {
+        var theNotifications = {
+            NotificationID: self.notificationdetails().NotificationID,
+            timeDate: self.notificationdetails().timeDate
+
+        };
+
+        if (confirm('Are you sure to delete "' + theNotifications.NotificationID + '"?')) {
+            $.ajax({
+                type: 'DELETE',
+                url: NotificationsURI + theNotifications.NotificationID,
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (data) {
+                    self.Notifications.remove(theNotifications);
+                    alert("Record has been deleted!");
+                },
+                error: function (err) {
+                    alert("Error: " + err.status + " " + err.statusText);
+                }
+            });
+        };
+    };
+
+
+
+
+    var UsersURI = 'http://localhost:60854/api/Users/';
+
+    function getAllUsers() {
+        $.ajax({
+            type: 'GET',
+            url: UsersURI,
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.user(data);
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+    }
+
+
+    getAllUsers();
+
+    self.newUser = {
+        userID: ko.observable(),
+        email: ko.observable(),
+        user: ko.observable(),
+        password: ko.observable(),
+        number: ko.observable()
+    };
+
+    self.addUser = function () {
+        var addUser = {
+            userID: self.newUser.userID(),
+            email: self.newUser.email(),
+            user: self.newUser.user(),
+            password: self.newUser.password(),
+            number: self.newUser.number()
+        };
+        $.ajax({
+            type: 'POST',
+            url: UsersURI,
+            data: JSON.stringify(newUser),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                self.user.push(data);
+                alert("User added successfully!");
+            },
+            error: function (err) {
+                alert("Error: " + err.status + " " + err.statusText);
+            }
+        });
+
+    };
+
+    self.removeUser = function () {
+        var theUser = {
+            userID: self.userdetails().userID,
+            email: self.userdetails().email,
+            user: self.userdetails().user,
+            password: self.userdetails().password,
+            number: self.userdetails().number,
+        };
+        if (confirm('Are you sure to delete "' + theUser.userID + '"?')) {
+            $.ajax({
+                type: 'DELETE',
+                url: UsersURI + theUser.userID,
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (data) {
+                    self.activities.remove(theUser);
+                    alert("Record has been deleted!");
+                },
+                error: function (err) {
+                    alert("Error: " + err.status + " " + err.statusText);
+                }
+            });
+        }
+    };
+
+}
 ko.applyBindings(new ViewModel());
